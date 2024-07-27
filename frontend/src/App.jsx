e158dab7bd6d1fee1a67265d1c6840c4a07e547b
JasperCheng10 <jaspercheng10@gmail.com>
2024-07-27 12:30:07 -0400

    Interactive Map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import ReactMapGL from "react-map-gl";
+import Map from "react-map-gl"
+
+import "./App.css";
+
+const TOKEN = import.meta.env.VITE_MAPBOX_TOKEN;
+console.log(TOKEN)
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [viewPort, setViewPort] = useState({
+    latitude: 40.7128,
+    longitude: -74.006,
+    zoom: 10,
+    width: "100vw",
+    height: "100vh",
+  });
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Map
+        mapboxAccessToken={TOKEN}
+        initialViewState={{
+          longitude: -74.006,
+          latitude: 40.7128,
+          zoom: 10
+        }}
+        style={{width:600, height: 400}}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
