c8d74b17fdd7305415b7edf63f3baa328917d7d2
Brandon Bane <brandon.bane17@gmail.com>
2024-07-27 21:15:09 -0400

    frontend functions

--- frontend/src/components/ui/SearchBar.jsx
+++ frontend/src/components/ui/SearchBar.jsx
@@ -1,53 +1,33 @@
-import React, { useState } from "react";
-import Search from "../form/Search.jsx";
+import React, { useState } from 'react';
 
-export default function SearchBar() {
-  const [cityName, setCityName] = useState("");
-  const [distance, setDistance] = useState("");
+const SearchBar = ({ onSearch }) => {
+  const [cityState, setCityState] = useState('');
+  const [radius, setRadius] = useState('');
 
-  // Handles changes within the search bar
-  const handleChange = (e) => {
-    setCityName(e.target.value);
-  };
-
-    // Handles changes for the dropdown
-  const handleValue = (e) => {
-    setDistance(e.target.value);
-  };
-
-  // Console logs the inputted values
-  const onSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(cityName);
-    console.log(distance);
+    onSearch(cityState, radius);
   };
 
   return (
-    <div className="">
-      <form id="form" onSubmit={onSubmit} className="max-w-lg mx-auto">
-        <div className="flex">
-          <div className="flex relative w-full items-center">
-            <Search value={cityName} onChange={handleChange} />
-            <div className="">
-              <select
-                className={`p-2.5 ms-2 text-sm bg-gray-50 rounded-lg border hover:bg-gray-100 focus:ring-4 focus:outline-none ${
-                  distance === "" ? "text-gray-400" : "text-gray-900"
-                }`}
-                onChange={handleValue}
-                value={distance}
-              >
-                <option value="" disabled>
-                  Select distance
-                </option>
-                <option value={5}>5 miles</option>
-                <option value={10}>10 miles</option>
-                <option value={20}>20 miles</option>
-                <option value={50}>50 miles</option>
-              </select>
-            </div>
-          </div>
-        </div>
-      </form>
-    </div>
+    <form id="search-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={cityState}
+        onChange={(e) => setCityState(e.target.value)}
+        placeholder="Enter City and State"
+        required
+      />
+      <select value={radius} onChange={(e) => setRadius(e.target.value)} required>
+        <option value="" disabled>Select distance</option>
+        <option value="5">5 miles</option>
+        <option value="10">10 miles</option>
+        <option value="20">20 miles</option>
+        <option value="50">50 miles</option>
+      </select>
+      <button type="submit">Search</button>
+    </form>
   );
-}
+};
+
+export default SearchBar;
